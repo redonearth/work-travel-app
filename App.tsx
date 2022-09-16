208dc486528decd07082af275855ea93e8abe69d
Redonearth <wona23@gmail.com>
2022-09-16 17:52:57 +0900

    Work/Travel 탭 전환 기능 추가

--- App.tsx
+++ App.tsx
@@ -1,11 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
+import { theme } from './colors';
+
+interface IPressableProps {
+  pressed: boolean;
+}
 
 export default function App() {
+  const [working, setWorking] = useState(true);
+  const travel = () => setWorking(false);
+  const work = () => setWorking(true);
+  const handlePressable = ({ pressed }: IPressableProps) => [
+    { opacity: pressed ? 0.2 : 1 },
+  ];
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      <View style={styles.header}>
+        <Pressable style={handlePressable} onPress={work}>
+          <Text
+            style={{ ...styles.btnText, color: working ? 'white' : theme.grey }}
+          >
+            Work
+          </Text>
+        </Pressable>
+        <Pressable style={handlePressable} onPress={travel}>
+          <Text
+            style={{
+              ...styles.btnText,
+              color: !working ? 'white' : theme.grey,
+            }}
+          >
+            Travel
+          </Text>
+        </Pressable>
+      </View>
     </View>
   );
 }
@@ -13,8 +43,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: theme.bg,
+    paddingHorizontal: 20,
+  },
+  header: {
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+    marginTop: 100,
+  },
+  btnText: {
+    fontSize: 36,
+    fontWeight: '600',
   },
 });
